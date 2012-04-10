3cdf745327701c7f301a6930f36a6d9b40c9062b
Larry Knibb <larry.knibb@gmail.com>
2012-04-10 18:46:25 +0800

    update assembly properties - add description and shorten version number

--- SevenZip.Compression.LZMA.WindowsPhone/Properties/AssemblyInfo.cs
+++ SevenZip.Compression.LZMA.WindowsPhone/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SevenZip.Compression.LZMA.WindowsPhone")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Windows Phone library for LZMA decompression, based on the LZMA SDK v9.22 beta.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SevenZip.Compression.LZMA.WindowsPhone")]
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
